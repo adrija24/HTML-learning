9a6b827f045755d75edd77705d2b974c16cc7bf6
adrija24 <24adrijaroy@gmail.com>
2023-07-17 23:41:28 +0530

    speed and accuracy updated, level checked

--- astro-speed-test/src/assets/js/levelData.js
+++ astro-speed-test/src/assets/js/levelData.js
@@ -2,101 +2,121 @@ const levelData = [
     {
         level: 1,
         speed: 20,
+        finalAccuracy: 90,
     },
 
     {
         level: 2,
         speed: 22,
+        finalAccuracy: 90, 
     },
 
     {
         level: 3,
         speed: 24,
+        finalAccuracy: 91,
     },
 
     {
         level: 4,
         speed: 26,
+        finalAccuracy: 91,
     },
 
     {
         level: 5,
         speed: 28,
+        finalAccuracy: 92,
     },
 
     {
         level: 6,
         speed: 30,
+        finalAccuracy: 92,
     },
 
     {
         level: 7,
         speed: 32,
+        finalAccuracy: 93,
     },
 
     {
         level: 8,
         speed: 34,
+        finalAccuracy: 93,
     },
 
     {
         level: 9,
         speed: 36,
+        finalAccuracy: 94,
     },
 
     {
         level: 10,
         speed: 38,
+        finalAccuracy: 94,
     },
 
     {
         level: 11,
         speed: 40,
+        finalAccuracy: 95,
     },
 
     {
         level: 12,
         speed: 42,
+        finalAccuracy: 95,
     },
 
     {
         level: 13,
         speed: 44,
+        finalAccuracy: 95,
     },
 
     {
         level: 14,
         speed: 46,
+        finalAccuracy: 95,
     },
 
     {
         level: 15,
         speed: 48,
+        finalAccuracy: 96,
     },
 
     {
         level: 16,
         speed: 50,
+        finalAccuracy: 97,
     },
 
     {
         level: 17,
         speed: 52,
+        finalAccuracy: 98,
     },
 
     {
         level: 18,
         speed: 53,
+        finalAccuracy: 99,
     },
 
     {
         level: 19,
         speed: 54,
+        finalAccuracy: 100,
     },
 
     {
         level: 20,
         speed: 55,
+        finalAccuracy: 100,
     }
 ];
 export default levelData
