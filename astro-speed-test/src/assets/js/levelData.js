404ce07c58ec77e9cb4ab5b11a7071fdcddcf2f5
adrija24 <24adrijaroy@gmail.com>
2023-07-29 20:30:34 +0530

    API replaced & bug fixed
    API changed, redirect script moved into level page, localstorage issue for first time rendering fixed

--- astro-speed-test/src/assets/js/levelData.js
+++ astro-speed-test/src/assets/js/levelData.js
@@ -103,20 +103,201 @@ const levelData = [
 
     {
         level: 18,
-        speed: 53,
+        speed: 54,
         finalAccuracy: 99,
     },
 
     {
         level: 19,
-        speed: 54,
+        speed: 56,
         finalAccuracy: 100,
     },
 
     {
         level: 20,
-        speed: 55,
+        speed: 58,
         finalAccuracy: 100,
-    }
+    },
+
+    {
+    level: 21,
+    speed: 60,
+    finalAccuracy: 100,
+},
+
+{
+    level: 22,
+    speed: 62,
+    finalAccuracy: 100,
+},
+
+{
+    level: 23,
+    speed: 64,
+    finalAccuracy: 100,
+},
+
+{
+    level: 24,
+    speed: 66,
+    finalAccuracy: 100,
+},
+
+{
+    level: 25,
+    speed: 68,
+    finalAccuracy: 100,
+},
+
+{
+    level: 26,
+    speed: 70,
+    finalAccuracy: 100,
+},
+
+{
+    level: 27,
+    speed: 72,
+    finalAccuracy: 100,
+},
+
+{
+    level: 28,
+    speed: 74,
+    finalAccuracy: 100,
+},
+
+{
+    level: 29,
+    speed: 76,
+    finalAccuracy: 100,
+},
+
+{
+    level: 30,
+    speed: 78,
+    finalAccuracy: 100,
+},
+
+{
+    level: 31,
+    speed: 80,
+    finalAccuracy: 100,
+},
+
+{
+    level: 32,
+    speed: 82,
+    finalAccuracy: 100,
+},
+
+{
+    level: 33,
+    speed: 83,
+    finalAccuracy: 100,
+},
+
+{
+    level: 34,
+    speed: 84,
+    finalAccuracy: 100,
+},
+
+{
+    level: 35,
+    speed: 85,
+    finalAccuracy: 100,
+},
+
+{
+    level: 36,
+    speed: 86,
+    finalAccuracy: 100,
+},
+
+{
+    level: 37,
+    speed: 87,
+    finalAccuracy: 100,
+},
+
+{
+    level: 38,
+    speed: 88,
+    finalAccuracy: 100,
+},
+
+{
+    level: 39,
+    speed: 89,
+    finalAccuracy: 100,
+},
+
+{
+    level: 40,
+    speed: 90,
+    finalAccuracy: 100,
+},
+
+{
+    level: 41,
+    speed: 91,
+    finalAccuracy: 100,
+},
+
+{
+    level: 42,
+    speed: 92,
+    finalAccuracy: 100,
+},
+
+{
+    level: 43,
+    speed: 93,
+    finalAccuracy: 100,
+},
+
+{
+    level: 44,
+    speed: 94,
+    finalAccuracy: 100,
+},
+
+{
+    level: 45,
+    speed: 95,
+    finalAccuracy: 100,
+},
+
+{
+    level: 46,
+    speed: 96,
+    finalAccuracy: 100,
+},
+
+{
+    level: 47,
+    speed: 97,
+    finalAccuracy: 100,
+},
+
+{
+    level: 48,
+    speed: 98,
+    finalAccuracy: 100,
+},
+
+{
+    level: 49,
+    speed: 99,
+    finalAccuracy: 100,
+},
+
+{
+    level: 50,
+    speed: 100,
+    finalAccuracy: 100,
+},
+
 ];
 export default levelData
